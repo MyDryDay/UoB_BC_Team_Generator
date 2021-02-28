7946c05bf4ea57266e6aec17c0cbffe254741ef2
George Cope <G.Cope13@gmail.com>
2021-02-28 12:22:29 +0000

    Defined a function to generate the last portion of HTML - closing tags & the script declarations - & append it to the end of the HTML generated using employeeProfiles function

--- index.js
+++ index.js
@@ -127,28 +127,28 @@ employeeProfiles = (obj) => {
     });
 }
 
-// function htmlFooter() {
-//     const scriptHTML = `
-//             </div>
-//         </main>  
+function htmlFooter() {
+    const scriptHTML = `
+            </div>
+        </main>  
                 
-//         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-//             integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-//             crossorigin="anonymous"></script>
-//         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-//             integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-//             crossorigin="anonymous"></script>
-//         <script src="https://kit.fontawesome.com/a9b8b749c3.js" crossorigin="anonymous"></script>
-//         </body>
-//     </html>
-//     `;
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+            crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/a9b8b749c3.js" crossorigin="anonymous"></script>
+        </body>
+    </html>
+    `;
 
-//     fs.appendFile("./dist/index.html", scriptHTML, function (error) {
-//         if (error) {
-//             console.log(error);
-//         };
-//     });
-//     console.log("Success");
-// }
+    fs.appendFile("./dist/index.html", scriptHTML, function (error) {
+        if (error) {
+            console.log(error);
+        };
+    });
+    console.log("Success");
+}
 
 // // Functions for client input
